43c212821559beeaeb2aee905802c6c1a5bb5a82
Aelish Boghani <boghaniaelish@gmail.com>
2020-11-27 22:24:29 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Text from './component/Text';
 import Teambar from './component/Teambar';
 import Joinbar from './component/Joinbar';
-import Card from './component/Card';
+
 
 
 
